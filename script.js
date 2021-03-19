949e624230157c282c8be7c97586e9cb9bc330e2
Bruno Reis <78049267+br3is@users.noreply.github.com>
2021-03-19 17:29:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+let elementoLista = document.querySelector('ul')
+let elementoInput = document.querySelector('input#tarefaForm')
+let elementoBotao = document.querySelector('button#button')
+
+let tarefasArray = JSON.parse(localStorage.getItem('list_tarefas')) || []
+
+function mostrarTarefas(){
+    elementoLista.innerHTML = ''
+
+    for (tarefa of tarefasArray){
+        let elementotarefa = document.createElement('li')
+        let textoTarefa = document.createTextNode(tarefa)
+
+        let elementoLink = document.createElement('button')
+        elementoLink.setAttribute('id', 'delete')
+        let linkText = document.createTextNode('')
+        elementoLink.appendChild(linkText)
+
+        let pos = tarefasArray.indexOf(tarefa)
+        elementoLink.setAttribute('onclick', `deletaTarefa(${pos})`)
+
+        elementotarefa.appendChild(textoTarefa)
+        elementoLista.appendChild(elementotarefa)
+        elementotarefa.appendChild(elementoLink)
+    }
+}
+
+mostrarTarefas()
+
+elementoBotao.onclick = addTarefa
+
+function addTarefa(){
+    if(elementoInput.value == 0){
+        window.alert('Adicione uma tarefa antes de continuar.')
+    }else{
+    let textoTarefaAdd = elementoInput.value
+
+    tarefasArray.push(textoTarefaAdd)
+
+    elementoInput.value = ''
+    mostrarTarefas()
+    salvarNoLocalStorage()
+    }
+}
+
+function deletaTarefa(pos){
+    tarefasArray.splice(pos, 1)
+    mostrarTarefas()
+    salvarNoLocalStorage()
+}
+
+function salvarNoLocalStorage(){
+    localStorage.setItem('list_tarefas', JSON.stringify(tarefasArray))
+}
+
+
